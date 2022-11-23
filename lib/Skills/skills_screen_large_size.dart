f352acee3df3853cbda720650623a8b6510c8a31
Ali Cagatay <aliccagatay@gmail.com>
2022-11-23 15:42:33 +0000

    Added the skills page to the website.

--- lib/Skills/skills_screen_large_size.dart
+++ lib/Skills/skills_screen_large_size.dart
@@ -112,20 +112,484 @@ class SkillsScreenLargeSize extends StatelessWidget {
       backgroundColor: Colors.black,
       body: ListView(
         children: <Widget>[
+          const Padding(
+            padding: EdgeInsets.only(top: 60, left: 30, right: 30),
+            child: Text(
+              "Tech Stack",
+              style: TextStyle(
+                fontSize: 50,
+                color: Colors.white,
+              ),
+            ),
+          ),
           Padding(
-            padding: const EdgeInsets.symmetric(vertical: 80, horizontal: 30),
+            padding:
+                const EdgeInsets.only(left: 30, right: 30, top: 40, bottom: 80),
             child: Card(
               color: Colors.grey[900],
-              child: const Padding(
-                padding: EdgeInsets.all(80),
-                child: Center(
-                  child: Text(
-                    "Coming soon. ",
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 30,
+              child: SizedBox(
+                width: MediaQuery.of(context).size.width,
+                height: 400,
+                child: ListView(
+                  scrollDirection: Axis.horizontal,
+                  children: <Widget>[
+                    Padding(
+                      padding: const EdgeInsets.all(60),
+                      child: SizedBox(
+                        width: 250,
+                        child: Card(
+                          color: Colors.grey[800],
+                          child: const Center(
+                            child: Text(
+                              'JavaScript',
+                              style: TextStyle(
+                                fontSize: 40,
+                                color: Colors.white,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
                     ),
-                  ),
+                    Padding(
+                      padding: const EdgeInsets.all(60),
+                      child: SizedBox(
+                        width: 250,
+                        child: Card(
+                          color: Colors.grey[800],
+                          child: const Center(
+                            child: Text(
+                              'Python',
+                              style: TextStyle(
+                                fontSize: 40,
+                                color: Colors.white,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(60),
+                      child: SizedBox(
+                        width: 250,
+                        child: Card(
+                          color: Colors.grey[800],
+                          child: const Center(
+                            child: Text(
+                              'Dart',
+                              style: TextStyle(
+                                fontSize: 40,
+                                color: Colors.white,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(60),
+                      child: SizedBox(
+                        width: 250,
+                        child: Card(
+                          color: Colors.grey[800],
+                          child: const Center(
+                            child: Text(
+                              'Java',
+                              style: TextStyle(
+                                fontSize: 40,
+                                color: Colors.white,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(60),
+                      child: SizedBox(
+                        width: 250,
+                        child: Card(
+                          color: Colors.grey[800],
+                          child: const Center(
+                            child: Text(
+                              'HTML',
+                              style: TextStyle(
+                                fontSize: 40,
+                                color: Colors.white,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(60),
+                      child: SizedBox(
+                        width: 250,
+                        child: Card(
+                          color: Colors.grey[800],
+                          child: const Center(
+                            child: Text(
+                              'CSS',
+                              style: TextStyle(
+                                fontSize: 40,
+                                color: Colors.white,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(60),
+                      child: SizedBox(
+                        width: 250,
+                        child: Card(
+                          color: Colors.grey[800],
+                          child: const Center(
+                            child: Text(
+                              'PostgreSQL',
+                              style: TextStyle(
+                                fontSize: 40,
+                                color: Colors.white,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(60),
+                      child: SizedBox(
+                        width: 250,
+                        child: Card(
+                          color: Colors.grey[800],
+                          child: const Center(
+                            child: Text(
+                              'Matlab',
+                              style: TextStyle(
+                                fontSize: 40,
+                                color: Colors.white,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+          const Padding(
+            padding: EdgeInsets.only(top: 60, left: 30, right: 30),
+            child: Text(
+              "Framework & Library Stack",
+              style: TextStyle(
+                fontSize: 50,
+                color: Colors.white,
+              ),
+            ),
+          ),
+          Padding(
+            padding:
+                const EdgeInsets.only(left: 30, right: 30, top: 40, bottom: 80),
+            child: Card(
+              color: Colors.grey[900],
+              child: SizedBox(
+                width: MediaQuery.of(context).size.width,
+                height: 400,
+                child: ListView(
+                  scrollDirection: Axis.horizontal,
+                  children: <Widget>[
+                    Padding(
+                      padding: const EdgeInsets.all(60),
+                      child: SizedBox(
+                        width: 250,
+                        child: Card(
+                          color: Colors.grey[800],
+                          child: const Center(
+                            child: Text(
+                              'Flutter',
+                              style: TextStyle(
+                                fontSize: 40,
+                                color: Colors.white,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(60),
+                      child: SizedBox(
+                        width: 250,
+                        child: Card(
+                          color: Colors.grey[800],
+                          child: const Center(
+                            child: Text(
+                              'Django',
+                              style: TextStyle(
+                                fontSize: 40,
+                                color: Colors.white,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(60),
+                      child: SizedBox(
+                        width: 250,
+                        child: Card(
+                          color: Colors.grey[800],
+                          child: const Center(
+                            child: Text(
+                              'React.js',
+                              style: TextStyle(
+                                fontSize: 40,
+                                color: Colors.white,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(60),
+                      child: SizedBox(
+                        width: 250,
+                        child: Card(
+                          color: Colors.grey[800],
+                          child: const Center(
+                            child: Text(
+                              'Tensorflow',
+                              style: TextStyle(
+                                fontSize: 40,
+                                color: Colors.white,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(60),
+                      child: SizedBox(
+                        width: 250,
+                        child: Card(
+                          color: Colors.grey[800],
+                          child: const Center(
+                            child: Text(
+                              'PyTorch',
+                              style: TextStyle(
+                                fontSize: 40,
+                                color: Colors.white,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(60),
+                      child: SizedBox(
+                        width: 250,
+                        child: Card(
+                          color: Colors.grey[800],
+                          child: const Center(
+                            child: Text(
+                              'NLTK',
+                              style: TextStyle(
+                                fontSize: 40,
+                                color: Colors.white,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+          const Padding(
+            padding: EdgeInsets.only(top: 60, left: 30, right: 30),
+            child: Text(
+              "Tool Stack",
+              style: TextStyle(
+                fontSize: 50,
+                color: Colors.white,
+              ),
+            ),
+          ),
+          Padding(
+            padding:
+                const EdgeInsets.only(left: 30, right: 30, top: 40, bottom: 80),
+            child: Card(
+              color: Colors.grey[900],
+              child: SizedBox(
+                width: MediaQuery.of(context).size.width,
+                height: 400,
+                child: ListView(
+                  scrollDirection: Axis.horizontal,
+                  children: <Widget>[
+                    Padding(
+                      padding: const EdgeInsets.all(60),
+                      child: SizedBox(
+                        width: 250,
+                        child: Card(
+                          color: Colors.grey[800],
+                          child: const Center(
+                            child: Text(
+                              'Git',
+                              style: TextStyle(
+                                fontSize: 40,
+                                color: Colors.white,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(60),
+                      child: SizedBox(
+                        width: 250,
+                        child: Card(
+                          color: Colors.grey[800],
+                          child: const Center(
+                            child: Text(
+                              'Docker',
+                              style: TextStyle(
+                                fontSize: 40,
+                                color: Colors.white,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(60),
+                      child: SizedBox(
+                        width: 250,
+                        child: Card(
+                          color: Colors.grey[800],
+                          child: const Center(
+                            child: Text(
+                              "JetBrains IDE's",
+                              style: TextStyle(
+                                fontSize: 30,
+                                color: Colors.white,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(60),
+                      child: SizedBox(
+                        width: 250,
+                        child: Card(
+                          color: Colors.grey[800],
+                          child: const Center(
+                            child: Text(
+                              'VS Code',
+                              style: TextStyle(
+                                fontSize: 40,
+                                color: Colors.white,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(60),
+                      child: SizedBox(
+                        width: 250,
+                        child: Card(
+                          color: Colors.grey[800],
+                          child: const Center(
+                            child: Text(
+                              'Postman',
+                              style: TextStyle(
+                                fontSize: 40,
+                                color: Colors.white,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+          const Padding(
+            padding: EdgeInsets.only(top: 60, left: 30, right: 30),
+            child: Text(
+              "Design Stack",
+              style: TextStyle(
+                fontSize: 50,
+                color: Colors.white,
+              ),
+            ),
+          ),
+          Padding(
+            padding:
+                const EdgeInsets.only(left: 30, right: 30, top: 40, bottom: 80),
+            child: Card(
+              color: Colors.grey[900],
+              child: SizedBox(
+                width: MediaQuery.of(context).size.width,
+                height: 400,
+                child: ListView(
+                  scrollDirection: Axis.horizontal,
+                  children: <Widget>[
+                    Padding(
+                      padding: const EdgeInsets.all(60),
+                      child: SizedBox(
+                        width: 250,
+                        child: Card(
+                          color: Colors.grey[800],
+                          child: const Center(
+                            child: Text(
+                              'Affinity Designer',
+                              style: TextStyle(
+                                fontSize: 30,
+                                color: Colors.white,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(60),
+                      child: SizedBox(
+                        width: 250,
+                        child: Card(
+                          color: Colors.grey[800],
+                          child: const Center(
+                            child: Text(
+                              'Keynote',
+                              style: TextStyle(
+                                fontSize: 40,
+                                color: Colors.white,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
                 ),
               ),
             ),
