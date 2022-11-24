217c8005f3c399f9017ba89509503d0e813f3ee6
Ali Cagatay <aliccagatay@gmail.com>
2022-11-24 21:03:36 +0000

    Added the experience screen.

--- lib/Experience/experience_screen_small_size.dart
+++ lib/Experience/experience_screen_small_size.dart
@@ -3,6 +3,7 @@ import 'package:personal_website_new/Contact/contact_screen.dart';
 import 'package:personal_website_new/HomeScreen/home_screen.dart';
 import 'package:personal_website_new/Projects/projects_screen.dart';
 import 'package:personal_website_new/Skills/skills_screen.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class ExperienceScreenSmallSize extends StatelessWidget {
   const ExperienceScreenSmallSize({super.key});
@@ -115,19 +116,73 @@ class ExperienceScreenSmallSize extends StatelessWidget {
       backgroundColor: Colors.black,
       body: ListView(
         children: <Widget>[
+          const Padding(
+            padding: EdgeInsets.only(top: 60, left: 30, right: 20),
+            child: Text(
+              "CodeYourFuture (September 2020 - Present)",
+              style: TextStyle(
+                fontSize: 33,
+                color: Colors.white,
+              ),
+            ),
+          ),
+          const Padding(
+            padding: EdgeInsets.only(top: 30, left: 40),
+            child: Text(
+              "Tech Lead & Teaching Assistant & Education Buddy Mentor",
+              style: TextStyle(
+                fontSize: 23,
+                color: Colors.white,
+              ),
+            ),
+          ),
           Padding(
-            padding: const EdgeInsets.symmetric(vertical: 80, horizontal: 30),
+            padding:
+                const EdgeInsets.only(left: 20, right: 20, top: 40, bottom: 80),
             child: Card(
               color: Colors.grey[900],
-              child: const Padding(
-                padding: EdgeInsets.all(80),
-                child: Center(
-                  child: Text(
-                    "Coming soon. ",
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 30,
-                    ),
+              child: SizedBox(
+                height: 400,
+                child: Padding(
+                  padding: const EdgeInsets.only(left: 20, right: 20),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      const Text(
+                        "At CodeYourFuture, I am working both as a technical assistant and technical lead and taught "
+                        "full stack web development to trainees consisting programming languages and frameworks such as "
+                        "HTML, CSS, JavaScript, React.js, Node and PostgreSQL.",
+                        style: TextStyle(
+                          fontSize: 18,
+                          color: Colors.white,
+                        ),
+                      ),
+                      const Text(
+                        "In addition, I am mentoring 3 or 4 students in the class specifically in their tech journey.",
+                        style: TextStyle(
+                          fontSize: 18,
+                          color: Colors.white,
+                        ),
+                      ),
+                      InkWell(
+                        onTap: () async {
+                          const url = "https://codeyourfuture.io";
+                          if (await canLaunchUrl(Uri.parse(url))) {
+                            await launchUrl(Uri.parse(url));
+                          } else {
+                            throw "Could not launch $url";
+                          }
+                        },
+                        child: const Text(
+                          "To learn more about who CodeYourFuture is, and what they do, press into this text.",
+                          style: TextStyle(
+                            fontSize: 18,
+                            color: Colors.white,
+                          ),
+                        ),
+                      )
+                    ],
                   ),
                 ),
               ),
