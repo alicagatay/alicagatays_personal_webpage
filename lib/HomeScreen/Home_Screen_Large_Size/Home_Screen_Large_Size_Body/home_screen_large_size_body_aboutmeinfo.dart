80684bf9ac7c1630dcfadd1e362f0976a39de4e8
Ali Cagatay <aliccagatay@gmail.com>
2023-02-26 15:00:15 +0000

    Updated `home_screen_large_size_body_aboutmeinfo.dart`.

--- lib/HomeScreen/Home_Screen_Large_Size/Home_Screen_Large_Size_Body/home_screen_large_size_body_aboutmeinfo.dart
+++ lib/HomeScreen/Home_Screen_Large_Size/Home_Screen_Large_Size_Body/home_screen_large_size_body_aboutmeinfo.dart
@@ -12,12 +12,22 @@ class HomeScreenLargeSizeBodyAboutMeInfo extends StatelessWidget {
         child: const Padding(
           padding: EdgeInsets.all(80),
           child: Text(
-            "Hi there, my name is Ali. "
-            "I am a software developer based in Birmingham, UK. "
-            "I am mainly interested in full stack web development and mobile application development. "
-            "In my free times, I love working out, listening to music, "
-            "and program. My favorite programming language is JavaScript, and my favorite framework "
-            "is Flutter.",
+            'Hi there, my name is Ali. I am a software engineer '
+            'from Birmingham, United Kingdom who generally enjoys '
+            'building stuff: be it a full stack web page, a cross '
+            'platform mobile application, or an API that people can '
+            'use on their backend services. I have strong technical '
+            'skills and background in software engineering, machine '
+            'learning and problem solving. In my university studies, '
+            'I have studied a variation of subjects such as machine '
+            'learning, data structures & algorithms, computer vision, '
+            'and mobile & ubiquitous computing. Other than '
+            'that, for the last 2 and a half years I am also volunteering at '
+            'CodeYourFuture, a school that offers career-focused software '
+            'development training to refugees and people from low-income '
+            'backgrounds, as a technical assistant and teaching lead in order '
+            'to teach full stack web development to its trainees using '
+            'technologies such as HTML, CSS, JavaScript, React.js, Node.js and PostgreSQL.',
             style: TextStyle(
               color: Colors.white,
               fontSize: 30,
