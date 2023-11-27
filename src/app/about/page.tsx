0f99351e1eef301fbd12ca164cbc1712e25bfe0d
Ali Cagatay <aliccagatay@gmail.com>
2023-11-27 18:35:19 +0000

    Update About page content and social links.

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -8,6 +8,7 @@ import {
   GitHubIcon,
   InstagramIcon,
   LinkedInIcon,
+  MediumIcon,
   TwitterIcon,
 } from '@/components/SocialIcons'
 import portraitImage from '@/images/portrait.jpg'
@@ -69,8 +70,8 @@ export default function About() {
         </div>
         <div className="lg:order-first lg:row-span-2">
           <h1 className="text-4xl font-bold tracking-tight text-zinc-800 dark:text-zinc-100 sm:text-5xl">
-            I’m Spencer Sharp. I live in New York City, where I design the
-            future.
+            I’m Ali Cagatay. I live in Birmingham, United Kingdom, where I
+            create the future by writing code.
           </h1>
           <div className="mt-6 space-y-7 text-base text-zinc-600 dark:text-zinc-400">
             <p>
@@ -103,24 +104,39 @@ export default function About() {
         </div>
         <div className="lg:pl-20">
           <ul role="list">
-            <SocialLink href="#" icon={TwitterIcon}>
-              Follow on Twitter
+            <SocialLink
+              href="https://medium.com/@aliccagatay"
+              icon={MediumIcon}
+            >
+              Follow on Medium
             </SocialLink>
-            <SocialLink href="#" icon={InstagramIcon} className="mt-4">
+            <SocialLink
+              href="https://www.instagram.com/_alicagatay/"
+              icon={InstagramIcon}
+              className="mt-4"
+            >
               Follow on Instagram
             </SocialLink>
-            <SocialLink href="#" icon={GitHubIcon} className="mt-4">
+            <SocialLink
+              href="https://github.com/alicagatay"
+              icon={GitHubIcon}
+              className="mt-4"
+            >
               Follow on GitHub
             </SocialLink>
-            <SocialLink href="#" icon={LinkedInIcon} className="mt-4">
+            <SocialLink
+              href="https://www.linkedin.com/in/alicagatay/"
+              icon={LinkedInIcon}
+              className="mt-4"
+            >
               Follow on LinkedIn
             </SocialLink>
             <SocialLink
-              href="mailto:spencer@planetaria.tech"
+              href="mailto:aliccagatay@gmail.com"
               icon={MailIcon}
               className="mt-8 border-t border-zinc-100 pt-8 dark:border-zinc-700/40"
             >
-              spencer@planetaria.tech
+              aliccagatay@gmail.com
             </SocialLink>
           </ul>
         </div>
