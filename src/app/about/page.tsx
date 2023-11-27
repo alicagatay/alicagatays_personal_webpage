94caf59a8679e9c9f8b231953ff7acfed1ea769a
Ali Cagatay <aliccagatay@gmail.com>
2023-11-27 21:20:22 +0000

    Update About page content.

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -74,31 +74,44 @@ export default function About() {
             create the future by writing code.
           </h1>
           <div className="mt-6 space-y-7 text-base text-zinc-600 dark:text-zinc-400">
+            {/* I’m Ali, a full stack software engineer specialising in web and
+            mobile application development. I’m currently located in Birmingham,
+            United Kingdom. I am also a teaching lead at CodeYourFuture,
+            teaching full stack web development to disadvantaged people in the
+            United Kingdom, such as refugees, asylum seekers and people from low
+            income backgrounds. I also have a blog on Medium where I write about
+            full-stack web development. */}
             <p>
-              I’ve loved making things for as long as I can remember, and wrote
-              my first program when I was 6 years old, just two weeks after my
-              mom brought home the brand new Macintosh LC 550 that I taught
-              myself to type on.
+              I’ve loved making things for as long as I can remember, and
+              created my first program when I was 14 years old, a retro snake
+              game in Scratch. From that moment on, I knew I wanted to be a
+              software engineer, so I started learning how to code in my free
+              time with a programming language called Processing.
             </p>
             <p>
-              The only thing I loved more than computers as a kid was space.
-              When I was 8, I climbed the 40-foot oak tree at the back of our
-              yard while wearing my older sister’s motorcycle helmet, counted
-              down from three, and jumped — hoping the tree was tall enough that
-              with just a bit of momentum I’d be able to get to orbit.
+              I then went on to study Computer Science at the University of
+              Birmingham, where I learned about the fundamentals of software
+              engineering, data structures and algorithms, machine learning,
+              artificial intelligence and mobile & ubiquitous computing. I also
+              worked on numerous of interesting projects, such as a computer
+              vision algorithm that classifies brain scans to detect brain
+              tumours, or a mobile application that uses machine learning and
+              natural language processing to recommend workouts to users based
+              on their messages.
             </p>
             <p>
-              I spent the next few summers indoors working on a rocket design,
-              while I recovered from the multiple surgeries it took to fix my
-              badly broken legs. It took nine iterations, but when I was 15 I
-              sent my dad’s Blackberry into orbit and was able to transmit a
-              photo back down to our family computer from space.
+              Today, I’m a full-stack software engineer specialising in web and
+              mobile application development using tools such as Next.js,
+              React.js, TypeScript, Tailwind CSS and Flutter. In addition to
+              this, I am also a volunteer in a non-profit organisation called
+              CodeYourFuture as a teaching lead, where I teach full-stack web
+              development to people from disadvantaged backgrounds, such as
+              refugees, asylum seekers and people from low income backgrounds.
             </p>
             <p>
-              Today, I’m the founder of Planetaria, where we’re working on
-              civilian space suits and manned shuttle kits you can assemble at
-              home so that the next generation of kids really <em>can</em> make
-              it to orbit — from the comfort of their own backyards.
+              In addition, I have a blog on Medium where I constantly write
+              about the tools used in full-stack web and mobile application in a
+              series called <em>“The Mastery Series”</em>.
             </p>
           </div>
         </div>
