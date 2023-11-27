20ebb6257f4130928a2beb0cd94bc5ca8bfbdb84
Ali Cagatay <aliccagatay@gmail.com>
2023-11-27 18:07:14 +0000

    Update metadata title and description.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,12 +7,11 @@ import '@/styles/tailwind.css'
 
 export const metadata: Metadata = {
   title: {
-    template: '%s - Spencer Sharp',
-    default:
-      'Spencer Sharp - Software designer, founder, and amateur astronaut',
+    template: '%s - Ali Cagatay',
+    default: 'Ali Cagatay - Software engineer, writer, and full-time learner.',
   },
   description:
-    'I’m Spencer, a software designer and entrepreneur based in New York City. I’m the founder and CEO of Planetaria, where we develop technologies that empower regular people to explore space on their own terms.',
+    'I’m Ali, a software engineer based in Birmingham, United Kingdom.',
   alternates: {
     types: {
       'application/rss+xml': `${process.env.NEXT_PUBLIC_SITE_URL}/feed.xml`,
