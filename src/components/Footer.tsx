2d2910d96340547b56cf3f9fbce7ecee77311540
Ali Cagatay <aliccagatay@gmail.com>
2023-11-27 17:36:27 +0000

    Update Footer links.

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -29,8 +29,8 @@ export function Footer() {
               <div className="flex flex-wrap justify-center gap-x-6 gap-y-1 text-sm font-medium text-zinc-800 dark:text-zinc-200">
                 <NavLink href="/about">About</NavLink>
                 <NavLink href="/projects">Projects</NavLink>
-                <NavLink href="/speaking">Work</NavLink>
-                <NavLink href="/uses">Tech Stack</NavLink>
+                <NavLink href="/work">Work</NavLink>
+                <NavLink href="/tech-stack">Tech Stack</NavLink>
               </div>
               <p className="text-sm text-zinc-400 dark:text-zinc-500">
                 &copy; {new Date().getFullYear()} Spencer Sharp. All rights
