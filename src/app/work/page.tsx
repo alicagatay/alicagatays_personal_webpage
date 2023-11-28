1a0cacc3b0a4e87e8a5b338403057263cc0b54c3
Ali Cagatay <aliccagatay@gmail.com>
2023-11-28 12:52:43 +0000

    Add CodeYourFuture to your work history.

--- src/app/work/page.tsx
+++ src/app/work/page.tsx
@@ -52,7 +52,17 @@ export default function Speaking() {
       title="The history of all the companies I’ve been in and the projects I’ve worked on."
       intro="This list includes internships and volunteering work as well."
     >
-      <div className="space-y-20"></div>
+      <div className="space-y-20">
+        <SpeakingSection title="CodeYourFuture">
+          <Appearance
+            href="https://codeyourfuture.io/"
+            title="Teaching Lead & Technical Assistant"
+            description="A technical deep-dive into HelioStream, the real-time streaming library I wrote for transmitting live video back to Earth."
+            event="September 2020 - Present"
+            cta="Learn more about CodeYourFuture and the work they do"
+          />
+        </SpeakingSection>
+      </div>
     </SimpleLayout>
   )
 }
