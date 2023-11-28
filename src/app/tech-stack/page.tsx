cf7e41e606740621d345a2b944081ace4ce18d44
Ali Cagatay <aliccagatay@gmail.com>
2023-11-28 11:35:00 +0000

    Remove pre written Workstation tool content.

--- src/app/tech-stack/page.tsx
+++ src/app/tech-stack/page.tsx
@@ -35,7 +35,7 @@ function Tool({
 }
 
 export const metadata = {
-  title: 'Uses',
+  title: 'Tech Stack',
   description: 'Software I use, gadgets I love, and other things I recommend.',
 }
 
@@ -43,36 +43,10 @@ export default function Uses() {
   return (
     <SimpleLayout
       title="Software I use, gadgets I love, and other things I recommend."
-      intro="I get asked a lot about the things I use to build software, stay productive, or buy to fool myself into thinking I’m being productive when I’m really just procrastinating. Here’s a big list of all of my favorite stuff."
+      intro="A list of the tools and gadgets I use to build, design and test software + the tools I use to stay productive all day + other random stuff that I just really like in my workflow."
     >
       <div className="space-y-20">
-        <ToolsSection title="Workstation">
-          <Tool title="16” MacBook Pro, M1 Max, 64GB RAM (2021)">
-            I was using an Intel-based 16” MacBook Pro prior to this and the
-            difference is night and day. I’ve never heard the fans turn on a
-            single time, even under the incredibly heavy loads I put it through
-            with our various launch simulations.
-          </Tool>
-          <Tool title="Apple Pro Display XDR (Standard Glass)">
-            The only display on the market if you want something HiDPI and
-            bigger than 27”. When you’re working at planetary scale, every pixel
-            you can get counts.
-          </Tool>
-          <Tool title="IBM Model M SSK Industrial Keyboard">
-            They don’t make keyboards the way they used to. I buy these any time
-            I see them go up for sale and keep them in storage in case I need
-            parts or need to retire my main.
-          </Tool>
-          <Tool title="Apple Magic Trackpad">
-            Something about all the gestures makes me feel like a wizard with
-            special powers. I really like feeling like a wizard with special
-            powers.
-          </Tool>
-          <Tool title="Herman Miller Aeron Chair">
-            If I’m going to slouch in the worst ergonomic position imaginable
-            all day, I might as well do it in an expensive chair.
-          </Tool>
-        </ToolsSection>
+        <ToolsSection title="Workstation"></ToolsSection>
         <ToolsSection title="Development tools">
           <Tool title="Sublime Text 4">
             I don’t care if it’s missing all of the fancy IDE features everyone
