e9d38785aa2eb4d28ff282e3b005afa955ec7c48
Ali Cagatay <aliccagatay@gmail.com>
2023-11-28 12:09:49 +0000

    Remove pre written content from the Productivity section.

--- src/app/tech-stack/page.tsx
+++ src/app/tech-stack/page.tsx
@@ -107,27 +107,7 @@ export default function Uses() {
             users and developers, which is always a plus.
           </Tool>
         </ToolsSection>
-        <ToolsSection title="Productivity">
-          <Tool title="Alfred">
-            It’s not the newest kid on the block but it’s still the fastest. The
-            Sublime Text of the application launcher world.
-          </Tool>
-          <Tool title="Reflect">
-            Using a daily notes system instead of trying to keep things
-            organized by topics has been super powerful for me. And with
-            Reflect, it’s still easy for me to keep all of that stuff
-            discoverable by topic even though all of my writing happens in the
-            daily note.
-          </Tool>
-          <Tool title="SavvyCal">
-            Great tool for scheduling meetings while protecting my calendar and
-            making sure I still have lots of time for deep work during the week.
-          </Tool>
-          <Tool title="Focus">
-            Simple tool for blocking distracting websites when I need to just do
-            the work and get some momentum going.
-          </Tool>
-        </ToolsSection>
+        <ToolsSection title="Productivity"></ToolsSection>
       </div>
     </SimpleLayout>
   )
