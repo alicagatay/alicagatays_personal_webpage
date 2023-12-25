c99eec1847e25467e30debb5deb786958ea87abc
Ali Cagatay <aliccagatay@gmail.com>
2023-12-25 14:45:09 +0300

    Update metadata in About page.

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -67,7 +67,7 @@ function CalendarIcon(props: React.ComponentPropsWithoutRef<'svg'>) {
 export const metadata: Metadata = {
   title: 'About',
   description:
-    'I’m Spencer Sharp. I live in New York City, where I design the future.',
+    'I’m Ali Cagatay. I live in Birmingham, where I code the future.',
 }
 
 export default function About() {
