fa6994e49a4384e6867504421f4f54ec8f487382
Ali Cagatay <aliccagatay@gmail.com>
2024-04-14 22:49:50 +0300

    Add "Work With Me" link to Footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -31,6 +31,7 @@ export function Footer() {
                 <NavLink href="/projects">Projects</NavLink>
                 <NavLink href="/work">Work</NavLink>
                 <NavLink href="/gear">Gear</NavLink>
+                <NavLink href="/work-with-me">Work With Me</NavLink>
               </div>
               <p className="text-sm text-zinc-400 dark:text-zinc-500">
                 &copy; {new Date().getFullYear()} Ali Cagatay. All rights
