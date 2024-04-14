c688a9010d7860c955488b5a5093d84aeecb9ba5
Ali Cagatay <aliccagatay@gmail.com>
2024-04-14 22:49:56 +0300

    Add "Work With Me" link to mobile and desktop navigation

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -136,6 +136,7 @@ function MobileNavigation(
                 <MobileNavItem href="/projects">Projects</MobileNavItem>
                 <MobileNavItem href="/work">Work</MobileNavItem>
                 <MobileNavItem href="/gear">Gear</MobileNavItem>
+                <MobileNavItem href="/work-with-me">Work With Me</MobileNavItem>
               </ul>
             </nav>
           </Popover.Panel>
@@ -177,11 +178,12 @@ function NavItem({
 function DesktopNavigation(props: React.ComponentPropsWithoutRef<'nav'>) {
   return (
     <nav {...props}>
-      <ul className="flex rounded-full bg-white/90 px-3 text-sm font-medium text-zinc-800 shadow-lg shadow-zinc-800/5 ring-1 ring-zinc-900/5 backdrop-blur dark:bg-zinc-800/90 dark:text-zinc-200 dark:ring-white/10">
+      <ul className="flex w-[400px] rounded-full bg-white/90 px-3 text-sm font-medium text-zinc-800 shadow-lg shadow-zinc-800/5 ring-1 ring-zinc-900/5 backdrop-blur dark:bg-zinc-800/90 dark:text-zinc-200 dark:ring-white/10">
         <NavItem href="/about">About</NavItem>
         <NavItem href="/projects">Projects</NavItem>
         <NavItem href="/work">Work</NavItem>
         <NavItem href="/gear">Gear</NavItem>
+        <NavItem href="/work-with-me">Work With Me</NavItem>
       </ul>
     </nav>
   )
