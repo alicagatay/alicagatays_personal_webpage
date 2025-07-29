3c942b045adf4c3dd6fbdd16a26a9b90b2717764
Ali Cagatay <aliccagatay@gmail.com>
2025-07-29 16:01:30 +0100

    fix: update heading on About page to reflect personal journey

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -87,8 +87,7 @@ export default function About() {
         </div>
         <div className="lg:order-first lg:row-span-2">
           <h1 className="text-4xl font-bold tracking-tight text-zinc-800 dark:text-zinc-100 sm:text-5xl">
-            I’m Ali Cagatay. I live in Birmingham, United Kingdom, where I
-            create the future by writing code.
+            My Path So Far
           </h1>
           <div className="mt-6 space-y-7 text-base text-zinc-600 dark:text-zinc-400">
             <p>
