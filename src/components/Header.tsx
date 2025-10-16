b5ab26f7f8b795ce40817183115ee701f5c98b13
Ali Cagatay <aliccagatay@gmail.com>
2025-10-16 10:06:59 +0100

    refactor: Complete website rebuild

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -134,7 +134,6 @@ function MobileNavigation(
               <ul className="-my-2 divide-y divide-zinc-100 text-base text-zinc-800 dark:divide-zinc-100/5 dark:text-zinc-300">
                 <MobileNavItem href="/about">About</MobileNavItem>
                 <MobileNavItem href="/projects">Projects</MobileNavItem>
-                <MobileNavItem href="/thought-log">Thought Log</MobileNavItem>
                 <MobileNavItem href="/education">Education</MobileNavItem>
                 <MobileNavItem href="/work">Work</MobileNavItem>
                 <MobileNavItem href="/hackathons">Hackathons</MobileNavItem>
@@ -181,10 +180,9 @@ function NavItem({
 function DesktopNavigation(props: React.ComponentPropsWithoutRef<'nav'>) {
   return (
     <nav {...props}>
-      <ul className="flex w-[600px] rounded-full bg-white/90 px-3 text-sm font-medium text-zinc-800 shadow-lg shadow-zinc-800/5 ring-1 ring-zinc-900/5 backdrop-blur dark:bg-zinc-800/90 dark:text-zinc-200 dark:ring-white/10">
+      <ul className="flex rounded-full bg-white/90 px-3 text-sm font-medium text-zinc-800 shadow-lg shadow-zinc-800/5 ring-1 ring-zinc-900/5 backdrop-blur dark:bg-zinc-800/90 dark:text-zinc-200 dark:ring-white/10">
         <NavItem href="/about">About</NavItem>
         <NavItem href="/projects">Projects</NavItem>
-        <NavItem href="/thought-log">Thought Log</NavItem>
         <NavItem href="/education">Education</NavItem>
         <NavItem href="/work">Work</NavItem>
         <NavItem href="/hackathons">Hackathons</NavItem>
