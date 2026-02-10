39a64b1ffb69e067177c3ddd4a0b496840431c52
Ali Cagatay <aliccagatay@gmail.com>
2026-02-10 10:48:12 +0000

    feat: update Link import to use custom Link component

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,6 +1,6 @@
 import { type Metadata } from 'next'
 import Image from 'next/image'
-import Link from 'next/link'
+import Link from '@/components/Link'
 import clsx from 'clsx'
 
 import { Container } from '@/components/Container'
