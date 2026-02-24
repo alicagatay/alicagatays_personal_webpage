6d9d8498ff18500f691bab19f20f49c95d1bb4d2
Ali Cagatay <aliccagatay@gmail.com>
2026-02-24 10:06:38 +0000

    feat: enhance NotFound page with internationalization support

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,7 +1,10 @@
 import { Button } from '@/components/Button'
 import { Container } from '@/components/Container'
+import { getTranslations } from 'next-intl/server'
 
-export default function NotFound() {
+export default async function NotFound() {
+  let t = await getTranslations('common')
+
   return (
     <Container className="flex h-full items-center pt-16 sm:pt-32">
       <div className="flex flex-col items-center">
@@ -9,13 +12,13 @@ export default function NotFound() {
           404
         </p>
         <h1 className="mt-4 text-4xl font-bold tracking-tight text-zinc-800 dark:text-zinc-100 sm:text-5xl">
-          Page not found
+          {t('notFound.title')}
         </h1>
         <p className="mt-4 text-base text-zinc-600 dark:text-zinc-400">
-          Sorry, we couldn’t find the page you’re looking for.
+          {t('notFound.description')}
         </p>
         <Button href="/" variant="secondary" className="mt-4">
-          Go back home
+          {t('notFound.goHome')}
         </Button>
       </div>
     </Container>
