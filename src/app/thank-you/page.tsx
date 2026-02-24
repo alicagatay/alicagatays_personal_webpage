b4097e2f1148da112da1e489ff312b66387db7c7
Ali Cagatay <aliccagatay@gmail.com>
2026-02-24 10:07:08 +0000

    feat: implement internationalization for Thank You page content

--- src/app/thank-you/page.tsx
+++ src/app/thank-you/page.tsx
@@ -1,4 +1,5 @@
 import { type Metadata } from 'next'
+import { getTranslations } from 'next-intl/server'
 
 import { SimpleLayout } from '@/components/SimpleLayout'
 
@@ -7,11 +8,8 @@ export const metadata: Metadata = {
   description: 'Thanks for subscribing to my newsletter.',
 }
 
-export default function ThankYou() {
-  return (
-    <SimpleLayout
-      title="Thanks for subscribing."
-      intro="I’ll send you an email any time I publish a new blog post, release a new project, or have anything interesting to share that I think you’d want to hear about. You can unsubscribe at any time, no hard feelings."
-    />
-  )
+export default async function ThankYou() {
+  let t = await getTranslations('thankYou')
+
+  return <SimpleLayout title={t('title')} intro={t('intro')} />
 }
